020f03db088761e4aa068b8b3e92136641e0bbe0
win_park <psk0812@github.com>
2023-08-10 10:54:04 +0900

    확인 엑스마스

--- Reboot scanner/xmas.py
+++ Reboot scanner/xmas.py
@@ -1,32 +1,20 @@
-import socket
+from scapy.all import IP, TCP, sr1
 
-def xmas_scan(target, port):
-    try:
-        # 소켓 생성
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(1)  # 타임아웃 설정
-        
-        # 포트 스캔 시도
-        s.connect((target, port))
-        
-        # XMAS 패킷 생성
-        xmas_packet = b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+def xmas_scan(target_ip, port):
+    # XMAS 플래그가 설정된 TCP 패킷 생성
+    packet = IP(dst=target_ip) / TCP(dport=port, flags="FPU")
 
-        # 패킷 전송
-        s.send(xmas_packet)
+    # 패킷 전송하고 응답 받기
+    response = sr1(packet, timeout=1, verbose=0)
 
-        # 응답 확인
-        response = s.recv(1024)
-        if not response:
-            return f"Port {port} is open"
+    if response:
+        tcp_layer = response.getlayer(TCP)
+        # RST 플래그가 설정된 응답이면 포트가 닫혀 있음
+        if tcp_layer and tcp_layer.flags == 0x4:
+            return f"Port {port} is closed."
         else:
-            return f"Port {port} is closed"
-        
-        # 소켓 닫기
-        s.close()
-    except socket.timeout:
-        return f"Port {port} is filtered"
-    except ConnectionRefusedError:
-        return f"Port {port} is closed"
+            return f"Port {port} is open|filtered."
+    else:
+        return f"Port {port} is filtered."
 
 
